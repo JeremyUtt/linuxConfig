42a4d90de86a76d3e9a879692db2b68ac263e32e
JeremyUtt <99843856+JeremyUtt@users.noreply.github.com>
2023-05-06 20:55:11 -0600

    Update UbuntuPostInstall.sh

--- UbuntuPostInstall.sh
+++ UbuntuPostInstall.sh
@@ -1,5 +1,3 @@
-#! /usr/bin/bash
-
 # ==================================
 # ===== Install ZSH/oh-my-zsh ======
 # ==================================
@@ -12,22 +10,28 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/too
 # ===========================
 # ===== Install Neovim ======
 # ===========================
+wait
 
 # install dependencies
 sudo apt-get install ninja-build gettext cmake unzip curl -y
 
-cd ~
+cd tmp
 git clone https://github.com/neovim/neovim
 cd neovim
 make CMAKE_BUILD_TYPE=Release # Build latest source
 
 cd build
-cpack -G DEB # package into Deb file
+
+if [ ! -f "nvim-linux64.deb" ]; then
+    cpack -G DEB # package into Deb file
+fi
+
 sudo dpkg -i nvim-linux64.deb # install Deb
 
 # ========================================
 # ===== Install Neovim kickstart.sh ======
 # ========================================
+wait
 
 # download nvim kickstart.sh
 mkdir ~/.config
