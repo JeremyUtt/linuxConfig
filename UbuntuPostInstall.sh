6901f4fecc29fea9b3a5863189c61a265c437458
JeremyUtt <99843856+JeremyUtt@users.noreply.github.com>
2023-05-06 19:16:24 -0600

    Update UbuntuPostInstall.sh

--- UbuntuPostInstall.sh
+++ UbuntuPostInstall.sh
@@ -5,9 +5,8 @@ sudo apt install neovim zsh
 # download oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-chsh -s $(which zsh)
-
+# chsh -s $(which zsh)
 
 # download nvim kickstart.sh
+mkdir ~/.config
 git clone https://github.com/nvim-lua/kickstart.nvim.git ~/.config/nvim
-
